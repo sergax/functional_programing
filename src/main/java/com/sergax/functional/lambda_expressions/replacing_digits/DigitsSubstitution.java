84709b491abc67292b24838cc4015794288c25fa
sergax <serey.aksenchenko@gmail.com>
2022-03-29 01:53:26 +0300

    updated

---
+++ src/main/java/com/sergax/functional/lambda_expressions/replacing_digits/DigitsSubstitution.java
@@ -0,0 +1,46 @@
+package com.sergax.functional.lambda_expressions.replacing_digits;
+
+import java.util.Scanner;
+
+/**
+ * by Aksenchenko Serhii on 29.03.2022
+ * <p>
+ * Replacing digits of a number
+ * In this task, you need to write your own rule and pass it to the substitution method
+ * to replace the digits of a number. The definition is exactly the same as shown in the previous step.
+ * <p>
+ * public static int substitution(long n, Function<Integer, Integer> digitRule)
+ * The rule should act as the following:
+ * <p>
+ * if the digit is 5, it should replace it with 1;
+ * if the digit is 6, it should replace it with 2;
+ * if the digit is 7, it should replace it with 3;
+ * if the digit is 8, it should replace it with 4;
+ * if the digit is 9, it should replace it with 5;
+ * any other digits shouldn't be replaced.
+ * There is a whole variety of ways to contrive this rule. The shorter your solution, the better! :)
+ * <p>
+ * Note that the substitution method belongs to the Utils class.
+ * <p>
+ * Sample Input:
+ * <p>
+ * 12765
+ * Sample Output:
+ * <p>
+ * 12321
+ */
+public class DigitsSubstitution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        long n = scanner.nextInt();
+
+        int result = Utils.substitution(n, digit -> digit == 5 ? 1 : digit);
+        int result2 = Utils.substitution(n, digit -> digit == 6 ? 2 : digit);
+        int result3 = Utils.substitution(n, digit -> digit == 7 ? 3 : digit);
+        int result4 = Utils.substitution(n, digit -> digit == 8 ? 4 : digit);
+        int result5 = Utils.substitution(n, digit -> digit == 9 ? 5 : digit);
+
+        System.out.println(result);
+    }
+}
---
+++ src/main/java/com/sergax/functional/lambda_expressions/replacing_digits/Utils.java
@@ -0,0 +1,20 @@
+package com.sergax.functional.lambda_expressions.replacing_digits;
+
+import java.util.function.Function;
+
+/**
+ * by Aksenchenko Serhii on 29.03.2022
+ */
+public class Utils {
+    public static int substitution(long n, Function<Integer, Integer> digitRule) {
+        String numberAsString = String.valueOf(n);
+        int result = 0;
+
+        for (int i = 0; i < numberAsString.length(); i++) {
+            result *= 10;
+            int nextDigit = Integer.parseInt(String.valueOf(numberAsString.charAt(i)));
+            result += digitRule.apply(nextDigit);
+        }
+        return result;
+    }
+}

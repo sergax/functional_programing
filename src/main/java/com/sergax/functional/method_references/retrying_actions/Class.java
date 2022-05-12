9d20b57b58a2deebe0e2463e9190e13a03839dd0
sergax <serey.aksenchenko@gmail.com>
2022-05-13 00:19:06 +0300

    add exmpl method reference

---
+++ src/main/java/com/sergax/functional/method_references/retrying_actions/Class.java
@@ -0,0 +1,18 @@
+package com.sergax.functional.method_references.retrying_actions;
+
+import java.util.function.Function;
+
+/**
+ * by Aksenchenko Serhii on 12.05.2022
+ */
+public class Class {
+    public static void main(String[] args) {
+        Function<String, Integer> parser = Integer::parseInt;
+
+        int i = parser.apply("123");
+//        int j = parser.apply("acv");
+
+        System.out.println(i);
+       // System.out.println(j);
+    }
+}

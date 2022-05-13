fed5656f4765abe386a0c385bb955f05cce4fe80
sergax <serey.aksenchenko@gmail.com>
2022-05-13 12:10:22 +0300

    add filter account

---
+++ src/main/java/com/sergax/functional/method_references/filtering_accounts/Account.java
@@ -0,0 +1,4 @@
+package com.sergax.functional.method_references.filtering_accounts;/**
+* by Aksenchenko Serhii on 13.05.2022
+*/public class Account {
+}
---
+++ src/main/java/com/sergax/functional/method_references/filtering_accounts/Main.java
@@ -0,0 +1,7 @@
+package com.sergax.functional.method_references.filtering_accounts;
+
+/**
+ * by Aksenchenko Serhii on 13.05.2022
+ */
+public class Main {
+}

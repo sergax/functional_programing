e9879d7835c1a4f17dde8296d6ef61fc76042339
sergax <serey.aksenchenko@gmail.com>
2022-05-13 12:12:44 +0300

    add filter accounts

--- src/main/java/com/sergax/functional/method_references/filtering_accounts/Account.java
+++ src/main/java/com/sergax/functional/method_references/filtering_accounts/Account.java
@@ -1,4 +1,16 @@
-package com.sergax.functional.method_references.filtering_accounts;/**
-* by Aksenchenko Serhii on 13.05.2022
-*/public class Account {
+package com.sergax.functional.method_references.filtering_accounts;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+/**
+ * by Aksenchenko Serhii on 13.05.2022
+ */
+
+@AllArgsConstructor
+@Data
+public class Account {
+    private String number;
+    private long balance;
+    private boolean locked;
 }
--- src/main/java/com/sergax/functional/method_references/filtering_accounts/Main.java
+++ src/main/java/com/sergax/functional/method_references/filtering_accounts/Main.java
@@ -1,7 +1,47 @@
 package com.sergax.functional.method_references.filtering_accounts;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
 /**
  * by Aksenchenko Serhii on 13.05.2022
  */
 public class Main {
+    public static void printFilteredAccounts(List<Account> accounts) {
+        List<Account> nonEmptyAccounts = filter(accounts,
+                account -> account.getBalance() > 0);// write your code here
+
+        List<Account> accountsWithTooMuchMoney = filter(accounts,
+                account -> !account.isLocked() && account.getBalance() >= 100_000_000);
+        // write your code here
+
+        // Please, do not change the code below
+        nonEmptyAccounts.forEach(a -> System.out.print(a.getNumber() + " "));
+        accountsWithTooMuchMoney.forEach(a -> System.out.print(a.getNumber() + " "));
+    }
+
+    public static <T> List<T> filter(List<T> elems, Predicate<T> predicate) {
+        return elems.stream()
+                .filter(predicate)
+                .collect(Collectors.toList());
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+        List<Account> accounts = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            String[] values = scanner.nextLine().split("\\s+");
+            Account acc = new Account(
+                    values[0],
+                    Long.parseLong(values[1]),
+                    Boolean.parseBoolean(values[2])
+            );
+            accounts.add(acc);
+        }
+        printFilteredAccounts(accounts);
+    }
 }

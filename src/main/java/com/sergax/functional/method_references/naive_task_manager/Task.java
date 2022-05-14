e7bcab33d795230a1da3df3e414eb3902cc6fcdd
sergax <serey.aksenchenko@gmail.com>
2022-05-14 12:45:28 +0300

    add ternary predicate

---
+++ src/main/java/com/sergax/functional/method_references/naive_task_manager/Task.java
@@ -0,0 +1,4 @@
+package com.sergax.functional.method_references.naive_task_manager;/**
+* by Aksenchenko Serhii on 13.05.2022
+*/public class Task {
+}
---
+++ src/main/java/com/sergax/functional/method_references/ternary_pradicate/TernaryIntPredicate.java
@@ -0,0 +1,7 @@
+package com.sergax.functional.method_references.ternary_pradicate;
+
+/**
+ * by Aksenchenko Serhii on 14.05.2022
+ */
+public interface TernaryIntPredicate {
+}

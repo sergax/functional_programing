5ca1a8065433285ad35179f046e97de80e2d5620
sergax <sergey.aksenchenko@gmail.com>
2022-05-21 14:24:21 +0300

    add new class Account

--- src/main/java/com/sergax/functional/chapter_4/sorting_accounts/Account.java
+++ src/main/java/com/sergax/functional/chapter_4/sorting_accounts/Account.java
@@ -1,8 +1,5 @@
-package com.sergax.functional.chapter_4.sorting_accounts;
-
-/**
- * @author : sergax
- * @date : 21.05.22
- */
-public class Account {
+package com.sergax.functional.chapter_4.sorting_accounts;/**
+* @author : sergax
+* @date : 21.05.22
+*/public class Account {
 }

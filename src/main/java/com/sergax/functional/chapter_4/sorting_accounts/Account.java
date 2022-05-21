22b38c3efdadcbd2e55090aa45fc1da98ad82a8b
sergax <sergey.aksenchenko@gmail.com>
2022-05-21 14:25:44 +0300

    add new class Account

--- src/main/java/com/sergax/functional/chapter_4/sorting_accounts/Account.java
+++ src/main/java/com/sergax/functional/chapter_4/sorting_accounts/Account.java
@@ -1,5 +1,16 @@
-package com.sergax.functional.chapter_4.sorting_accounts;/**
-* @author : sergax
-* @date : 21.05.22
-*/public class Account {
+package com.sergax.functional.chapter_4.sorting_accounts;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+/**
+ * @author : sergax
+ * @date : 21.05.22
+ */
+@AllArgsConstructor
+@Data
+class Account {
+    private final String owner;
+    private final long balance;
+    private final boolean locked;
 }

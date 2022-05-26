08c07879237e22815ea6d537f1547bbea82d450f
sergax <sergey.aksenchenko@gmail.com>
2022-05-26 14:56:10 +0300

    add solution for Competition

---
+++ src/main/java/com/sergax/functional/chapter_5/competition/Team.java
@@ -0,0 +1,16 @@
+package com.sergax.functional.chapter_5.competition;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/**
+ * @author : sergax
+ * @date : 26.05.22
+ */
+
+@AllArgsConstructor
+@Getter
+public class Team {
+    private final String name;
+    private final int place;
+}

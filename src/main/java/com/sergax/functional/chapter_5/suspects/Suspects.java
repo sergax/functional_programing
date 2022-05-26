ad95b281ec3b3853a9b1cb2ceb51537f08ce80c6
sergax <sergey.aksenchenko@gmail.com>
2022-05-26 13:23:37 +0300

    add solution for Suspects

--- src/main/java/com/sergax/functional/chapter_5/suspects/Suspects.java
+++ src/main/java/com/sergax/functional/chapter_5/suspects/Suspects.java
@@ -2,12 +2,17 @@ package com.sergax.functional.chapter_5.suspects;
 
 import java.util.List;
 import java.util.Scanner;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 /**
  * @author : sergax
  * @date : 26.05.22
+ *
+ * Recently there was a high-profile crime in a city. Police found a criminal's torn business card which
+ * has "jo" written on it. Detectives suspect that this is a part of the criminal's first or last name.
+ *
+ * You need to write some code to reduce the number of suspects according to this clue to make the crime
+ * investigation much easier. In the resulting list, the suspects must be sorted alphabetically according to their names.
  */
 
 public class Suspects {

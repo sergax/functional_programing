bfd95b0ea56e567d476b95806322dacd329a5bb9
sergax <sergey.aksenchenko@gmail.com>
2022-05-27 15:24:29 +0300

    fix typo :)

--- src/main/java/com/sergax/functional/chapter_5/suspects/Suspects.java
+++ src/main/java/com/sergax/functional/chapter_5/suspects/Suspects.java
@@ -7,10 +7,10 @@ import java.util.stream.Stream;
 /**
  * @author : sergax
  * @date : 26.05.22
- *
+ * <p>
  * Recently there was a high-profile crime in a city. Police found a criminal's torn business card which
  * has "jo" written on it. Detectives suspect that this is a part of the criminal's first or last name.
- *
+ * <p>
  * You need to write some code to reduce the number of suspects according to this clue to make the crime
  * investigation much easier. In the resulting list, the suspects must be sorted alphabetically according to their names.
  */

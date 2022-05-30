c0b3ffa8b328186f79d88fa5be34986edaf7d584
sergax <sergey.aksenchenko@gmail.com>
2022-05-30 16:49:39 +0300

    add solution for Generating user

---
+++ src/main/java/com/sergax/functional/chapter_6/User.java
@@ -0,0 +1,16 @@
+package com.sergax.functional.chapter_6;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/**
+ * @author : sergax
+ * @date : 30.05.22
+ */
+
+@AllArgsConstructor
+@Getter
+public class User {
+    private final long id;
+    private final String email;
+}
---
+++ src/main/java/com/sergax/functional/chapter_6/Utils.java
@@ -0,0 +1,25 @@
+package com.sergax.functional.chapter_6;
+
+import lombok.NoArgsConstructor;
+
+import java.util.stream.Stream;
+
+/**
+ * @author : sergax
+ * @date : 30.05.22
+ *
+ * You need to implement a method named generateUsers that generates the specified number of users.
+ * A user has an id and an email, and the values of these fields must be unique among the generated
+ * users (without duplicates), e.g:
+ */
+
+@NoArgsConstructor
+public class Utils {
+    public static Stream<User> generateUsers(int numberOfUsers) {
+        return Stream.iterate(0, (id) -> id + 1)
+                .limit(numberOfUsers)
+                .map(id -> new User(id, String.format("user%s@gmail.com", id)))
+                .distinct();
+        // write your code here
+    }
+}

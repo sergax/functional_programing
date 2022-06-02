8e1e386733dd3259bf2a89f893fc8881934cd502
sergax <sergey.aksenchenko@gmail.com>
2022-06-02 10:47:04 +0300

    add solution for Adding extensions

--- src/main/java/com/sergax/functional/chapter_7/adding_extensions/ExtensionService.java
+++ src/main/java/com/sergax/functional/chapter_7/adding_extensions/ExtensionService.java
@@ -1,8 +1,23 @@
 package com.sergax.functional.chapter_7.adding_extensions;
 
+import java.util.function.Function;
+import java.util.function.Predicate;
+
 /**
  * @author : sergax
  * @date : 02.06.22
+ *
+ * Implement the addExtension method that accepts two string predicates and returns a function.
+ * The returning function should accept one argument and return the argument with
+ * .xml suffix if the argument matches the first predicate,
+ * .json suffix if the argument matches the second predicate, and the argument itself otherwise.
  */
-public class ExtensionService {
+
+class ExtensionService {
+    public static Function<String, String> addExtension(Predicate<String> xml, Predicate<String> json) {
+
+       return file ->
+               file + ((xml.test(file)) ? ".xml" : json.test(file) ? ".json" : "");
+        // write your code here
+    }
 }
--- src/main/java/com/sergax/functional/chapter_7/adding_extensions/Main.java
+++ src/main/java/com/sergax/functional/chapter_7/adding_extensions/Main.java
@@ -1,8 +1,19 @@
 package com.sergax.functional.chapter_7.adding_extensions;
 
+import java.util.function.Function;
+
+import static com.sergax.functional.chapter_7.adding_extensions.ExtensionService.addExtension;
+
 /**
  * @author : sergax
  * @date : 02.06.22
  */
+
 public class Main {
+    public static void main(String[] args) {
+        Function<String, String> func = addExtension(y->y.equals("First"), y->y.equals("Second"));
+        System.out.println(func.apply("First"));//should print First.xml
+        System.out.println(func.apply("Second"));//should print Second.json
+        System.out.println(func.apply("Otherwise"));//should print Otherwise
+    }
 }

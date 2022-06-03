6bd0fc0ae39b387e5a89d635c18b83b828971d62
sergax <sergey.aksenchenko@gmail.com>
2022-06-03 11:02:19 +0300

    add solution for Custom reduce

---
+++ src/main/java/com/sergax/functional/chapter_7/reducer_operator/CustomReducer.java
@@ -0,0 +1,50 @@
+package com.sergax.functional.chapter_7.reducer_operator;
+
+import java.util.Scanner;
+import java.util.function.BiFunction;
+import java.util.function.IntBinaryOperator;
+import java.util.stream.IntStream;
+
+/**
+ * @author : sergax
+ * @date : 02.06.22
+ */
+
+public class CustomReducer {
+    /**
+     * The operator combines all values in the given range into one value
+     * using combiner and initial value (seed)
+     */
+    public static final BiFunction<Integer, IntBinaryOperator, IntBinaryOperator> reduceIntOperator =
+            (Integer x, IntBinaryOperator y) ->
+                    (IntBinaryOperator) (left, right) ->
+                            IntStream.rangeClosed(left, right).reduce(x, (a, b) -> y.applyAsInt(a, b));
+    /**
+     * The operator calculates the sum in the given range (inclusively)
+     */
+    public static final IntBinaryOperator sumOperator = reduceIntOperator.apply(0, (x, y) -> x + y);
+
+    /**
+     * The operator calculates the product in the given range (inclusively)
+     */
+    public static final IntBinaryOperator productOperator = reduceIntOperator.apply(1, (a, b) -> a * b);
+    // write your code here
+
+    // Don't change the code below
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        String[] values = scanner.nextLine().split(" ");
+
+        int l = Integer.parseInt(values[0]);
+        int r = Integer.parseInt(values[1]);
+
+        int sumReducer = reduceIntOperator.apply(0, Integer::sum).applyAsInt(l, r);
+        int sum = sumOperator.applyAsInt(l, r);
+
+        int prodReducer = reduceIntOperator.apply(1, (x, y) -> x * y).applyAsInt(l, r);
+        int prod = productOperator.applyAsInt(l, r);
+
+        System.out.println(String.format("%d %d %d %d", sumReducer, sum, prodReducer, prod));
+    }
+}

61a4ee9a8355e2417086e15ef941dc1b29b96827
sergax <sergey.aksenchenko@gmail.com>
2022-06-04 13:14:03 +0300

    update class :)

--- src/main/java/com/sergax/functional/chapter_7/parallel_query_executor/QueryUtils.java
+++ src/main/java/com/sergax/functional/chapter_7/parallel_query_executor/QueryUtils.java
@@ -5,7 +5,6 @@ import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Function;
-import java.util.stream.Stream;
 
 /**
  * @author : sergax
